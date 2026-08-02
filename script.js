03f4deb392fcaf2390dc97229ac3c9fb3e17a294
Aarav Osborn <aarav.osborn@gmail.com>
2026-08-02 21:52:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function upDate(previewPic) {
+    console.log("Mouse over event triggered");
+
+    console.log("Alt text:", previewPic.alt);
+    console.log("Image source:", previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function undo() {
+    document.getElementById("image").style.backgroundImage = "url('')";
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+}
